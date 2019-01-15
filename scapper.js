14007d78687d927f6637d7bbb8cb2bdfea61e71c
Albert Qin <albert.qin@edu.uwaterloo.ca>
2019-01-15 16:06:44 -0500

    completed webscrapper for 1 page

--- scapper.js
+++ scapper.js
@@ -2,21 +2,23 @@ const rp = require('request-promise');
 const $ = require('cheerio');
 const url = 'https://listings.och.uwaterloo.ca/Listings/Search/Results?page=1&ps=50';
 
+function checkspace(str){
+  return !((str == '        ') || (str == ''));
+}
 rp(url)
 .then(function(html){
     //success!
-    var addresses = [];
+    var links = [];
 
     for(let i = 0; i < 50; i++){
-      addresses.push($('a[style="float:left;"]', html)[i].attribs.href);
+      links.push($('a[style="float:left;"]', html)[i].attribs.href);
     }
 
-    var cities = [];
-    $('span[id="pageHeaderPlaceHolderSpan"]').each(function(i, elem) {
-      cities[i] = $(this).text();
-      console.log($(this).text());
-    });
-    console.log($('span[id="pageHeaderPlaceHolderSpan"]').text());
+    var addresses = [];
+    addresses = $('a[style="float:left;"]', html).text().split("\n");
+    addresses = addresses.filter(checkspace);
+    addresses = addresses.map(s => s.trim());
+    console.log(links);
     console.log(addresses);
   })
   .catch(function(err){
